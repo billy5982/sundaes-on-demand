c8103e4b03a2489e274ab55c4199bb09d0f4285d
billy5982 <kuu0404@naver.com>
2023-09-13 21:41:46 +0900

    ✨ feat : 특정 조건에 의해 가시되는 popover test + 제작

--- src/pages/summary/SummaryForm.jsx
+++ src/pages/summary/SummaryForm.jsx
@@ -1,15 +1,27 @@
 import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Popover from 'react-bootstrap/Popover';
+
+const popover = (
+  <Popover id='popover-basic'>
+    <Popover.Body>no ice cream will actually be delivered</Popover.Body>
+  </Popover>
+);
 
 export default function SummaryForm() {
   const [checked, setChecked] = useState(false);
 
   const checkboxLabel = (
     <span>
-      I agree to <span style={{ color: 'blue' }}>Terms and Conditions</span>
+      I agree to
+      <OverlayTrigger placement='right' overlay={popover}>
+        <span style={{ color: 'blue' }}>Terms and Conditions</span>
+      </OverlayTrigger>
     </span>
   );
+
   return (
     <Form>
       <Form.Group controlId='terms-and-conditions'>
--- src/pages/summary/test/SummaryForm.test.js
+++ src/pages/summary/test/SummaryForm.test.js
@@ -17,7 +17,7 @@ test('Summary의 초기 상태', () => {
 test('SummaryForm의 체크박스와 버튼 상호작용 테스트', async () => {
   render(<SummaryForm />);
 
-  const user = await userEvent.setup();
+  const user = userEvent.setup();
   const summaryCheckBox = screen.getByRole('checkbox', { name: /Terms/i });
 
   const confirmBtn = screen.getByRole('button', { name: 'Confirm order' });
@@ -27,4 +27,26 @@ test('SummaryForm의 체크박스와 버튼 상호작용 테스트', async () =>
 
   await user.click(summaryCheckBox);
   expect(confirmBtn).toBeDisabled();
+});
+
+test('마우스 커서를 올렸을 때 팝오버가 표시된다', async () => {
+  const user = userEvent.setup();
+
+  render(<SummaryForm />);
+
+  // 존재하지 않으면 null 이 반환됨 -> 화면에 존재하지 않을 경우 query를 사용하는 이유가 될 것 같음, get-find를 에러를 반환함. 에러가 반환되면 실패로 처리 됨
+  const nullPopover = screen.queryByText(/no ice cream will actually be delivered/i);
+
+  // 페이지 로딩 시 팝업은 없는 상태 (toBeInTheDocument -> 문서에 존재하는 지)
+  expect(nullPopover).not.toBeInTheDocument();
+
+  // 마우스 호버시 팝오버가 보이는 지
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  await user.hover(termsAndConditions);
+  const hoverPopover = screen.getByText(/no ice cream will actually be delivered/i);
+  expect(hoverPopover).toBeInTheDocument();
+
+  // 마우스 호버에서 뗐을 떄 팝오버가 사라지는 지
+  await user.unhover(hoverPopover);
+  expect(hoverPopover).not.toBeInTheDocument();
 });
